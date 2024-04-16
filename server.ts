5bd2b29ee88e2b01219071ee9c681c26a7353e45
Alex <alexander.clayton.email@gmail.com>
2024-04-15 22:16:47 -0500

    connected to sqlserver

--- server.ts
+++ server.ts
@@ -1,9 +1,28 @@
 import express, { Request, Response } from "express";
-// import sql from "mssql";
+import sql from "mssql";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`API server is running at http://127.0.0.1:${PORT}`);
-});
+const dbConfig = {
+  user: "alex",
+  password: "password",
+  server: "localhost",
+  database: "battletechclans",
+  options: {
+    trustServerCertificate: true,
+  },
+};
+
+const startServer = async () => {
+  try {
+    await sql.connect(dbConfig);
+    app.listen(PORT, () => {
+      console.log(`API server is running at http://127.0.0.1:${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+startServer();
